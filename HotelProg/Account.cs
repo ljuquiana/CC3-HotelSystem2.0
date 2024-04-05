0f05cd28351f005a13f27dc79bb5417759e75baa
ljuquiana <156904219+ljuquiana@users.noreply.github.com>
2024-04-05 23:38:28 +0800

    Create Account.cs

---
+++ HotelProg/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProgram
+{
+    public class Account
+    {
+        private string AccountID {  get; set; }
+        private string Password {  get; set; }
+    }
+}
