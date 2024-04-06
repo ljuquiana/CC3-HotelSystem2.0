bae67e369901ede9fb4930465be5e2f0e8497395
ljuquiana <156904219+ljuquiana@users.noreply.github.com>
2024-04-06 09:48:38 +0800

    Update Receptionist.cs

---
+++ HotelProg/Receptionist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProgram
+{
+    public class Receptionist : User
+    {
+        public Receptionist(string name, string address, string email, int phoneNumber) : base(name, address, email, phoneNumber)
+        {
+
+        }
+
+        public void BookReservation(Guest guest, Reservation reservation)
+        {
+            guest.BookReservation(reservation);
+        }
+    }
+}
