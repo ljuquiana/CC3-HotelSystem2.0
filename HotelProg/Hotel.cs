a21f9281caf9b26539aaf5db15d07a39f4591358
ljuquiana <156904219+ljuquiana@users.noreply.github.com>
2024-04-06 09:44:28 +0800

    Update Hotel.cs

---
+++ HotelProg/Hotel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProgram
+{
+    public class Hotel
+    {
+        public string HotelName {  get; set; }
+        public string Location {  get; set; }
+        private List<HotelRoom> _allRooms;
+
+        public Hotel(string hotelName, string location, List<HotelRoom> rooms)
+        {
+            HotelName = hotelName;
+            Location = location;
+            _allRooms = rooms;
+        }
+
+        public Hotel()
+        {
+            _allRooms = new List<HotelRoom>();
+        }
+
+        public void DisplayAvailableRooms()
+        {
+            foreach (HotelRoom room in _allRooms)
+            {
+                Console.WriteLine("\t" + room.DisplayDetails());
+            }
+        }
+
+        public void DisplayBookedRooms()
+        {
+            foreach (HotelRoom room in _allRooms)
+            {    
+                if (room.Status == false)
+                {
+                    Console.WriteLine("\t" + room.DisplayDetails());
+                }
+            }
+        }
+
+        public void ReserveRoom(HotelRoom room)
+        {
+            room.Status = false;
+        }
+
+        public virtual string DisplayDetails()
+        {
+            return $"{HotelName} {Location} {_allRooms}";
+        }
+    }
+}
