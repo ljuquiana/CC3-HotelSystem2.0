91a3fb0772042c8a16f244a218c43e4709ddab58
ljuquiana <156904219+ljuquiana@users.noreply.github.com>
2024-04-06 09:46:59 +0800

    Update HotelRoom.cs

---
+++ HotelProg/HotelRoom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProgram
+{
+    public class HotelRoom
+    {
+        public int RoomNumber {  get; set; }
+        public RoomStyle Style { get; set; }
+        public bool Status { get; set; }
+        public decimal BookingPrice {  get; set; }
+
+        public HotelRoom(int roomNumber, RoomStyle style, bool status, decimal price)
+        {
+            RoomNumber = roomNumber;
+            Style = style;
+            Status = status;
+            BookingPrice = price;
+        }
+
+        public virtual string DisplayDetails()
+        {
+            return $"{RoomNumber} {Status} {BookingPrice}";
+        }
+    }
+}
