77fd3123cf9cea18921460fe77bab76571f4d1c9
ljuquiana <156904219+ljuquiana@users.noreply.github.com>
2024-04-06 09:45:30 +0800

    Update HotelManagementSystem.cs

---
+++ HotelProg/HotelManagementSystem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProgram
+{
+    public class HotelManagementSystem
+    {
+        private List<Hotel> _hotels;
+        private List<User> _users;
+        private List<Reservation> _reservations;
+
+        public HotelManagementSystem()
+        {
+            _hotels = new List<Hotel>();
+            _users = new List<User>();
+            _reservations = new List<Reservation>();
+        }
+
+        public void RegisterUser(User user)
+        {
+            _users.Add(user);
+        }
+
+        public void AddHotel(Hotel hotel)
+        {
+            _hotels.Add(hotel);
+        }
+        public void DisplayHotels()
+        {
+            Console.WriteLine("Hotels: ");
+            foreach( Hotel hotel in _hotels)
+            {
+                Console.WriteLine("\t" + hotel.DisplayDetails());
+            }
+        }
+        
+        public void BookReservation(Hotel hotel, HotelRoom room, Guest guest, DateTime startTime, DateTime endTime)
+        {
+            _hotels.Add(hotel);
+        }
+
+        public void DisplayReservationDetails(int reservationNumber)
+        {
+            Console.WriteLine("\t" + reservationNumber);
+        }
+    }
+}
