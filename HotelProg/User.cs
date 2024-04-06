02c5cc6a739eb73e045deb9547c1ffbb5d6f9b5b
ljuquiana <156904219+ljuquiana@users.noreply.github.com>
2024-04-06 09:50:40 +0800

    Update User.cs

---
+++ HotelProg/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProgram
+{
+    public class User
+    {
+        public string Name {  get; set; }
+        public string Address {  get; set; }
+        public string Email {  get; set; }
+        public int PhoneNumber {  get; set; }
+
+        public User(string name, string address, string email, int phoneNumber)
+        {
+            Name = name;
+            Address = address;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
