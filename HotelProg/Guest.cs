a23a9945522ee0fe599de033413f8d8be98dc20a
ljuquiana <156904219+ljuquiana@users.noreply.github.com>
2024-04-06 09:43:16 +0800

    Update Guest.cs

---
+++ HotelProg/Guest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProgram
+{
+    public class Guest : User
+    {
+        private List<Reservation> _reservations;
+        public int TotalBookedRooms { get; set; }
+
+        public Guest(string name, string address, string email, int phoneNumber) : base(name, address, email, phoneNumber)
+        {
+            _reservations = new List<Reservation>();
+        }
+
+        public void BookReservation(Reservation reservation)
+        {
+            _reservations.Add(reservation);
+        }
+
+        public void DisplayReservations()
+        {
+            foreach (Reservation reservation in _reservations)
+            {
+                Console.WriteLine("\t" + reservation.DisplayDetails());
+            }
+        }
+    }
+}
